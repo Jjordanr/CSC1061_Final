4ccc48a2f4631eb9c70ed4f457111a8a3e41ad7c
Kerplunkenator <150295958+Kerplunkenator@users.noreply.github.com>
2023-11-16 23:27:09 -0700

    Add files via upload
    See changelogs.
    Modified both Final and creatures.h

--- creatures.h
+++ creatures.h
@@ -1,14 +1,31 @@
 /*
-
+Changelog:
 
 10/31/23: today I added the stat modifier functionality, these are the ones that are added to rolls
 
+11 November 2023 16:00 - Mark Ardrey
+  Commented out the player class to define it in the main program
+    for easier manipulation.
+    (Note: may re-define player in this file instead, to streamline main.)
+
+15 November 2023 14:00 - Mark Ardrey
+  Uncommented out the player class, and added functions updateStats
+    and characterCreation.
+  Modified updateStats to function as intended.
+
+16 November 2023 23:00 - Mark Ardrey
+  Almost completed characterCreation except for the randomly generated character.
+  Might delete randomly generating a character and instead force the player
+    to make their own.
 */
 
 #ifndef H_creatures
 #define H_creatures
-   
+
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 class creatureType{
     public:
@@ -64,9 +81,9 @@ class creatureType{
         int defense;
 
         int strength;
-        int charisma;
-        int dexterity;
-        int wisdom;
+  int charisma; //maybe get rid of charisma
+  int dexterity; //maybe get rid of dexterity
+  int wisdom; //maybe get rid of wisdom
         int intelligence;
         int constitution;
 
@@ -216,20 +233,185 @@ class creatureType{
         opponent.changeHealth(damage);
     }
 
-class player : public creatureType{
-    public:
-        player();
-    private: 
+class player:creatureType
+{
+public:
+  //constructor player();
+  void editInventory();
+  int updateStats(int statPointsTotal);
+  void characterCreation();
+private:
+  int inventory[10]; //the player has 10 inventory slots
+  int specialty; //1 for mage, 2 for fighter
+  //string spell; //used if player has selected mage
+  int statPointsTotal;
 };
 
+//BEGINNING OF PLAYER FUNCTIONS
+
+/*
+  constructor definition
 player::player(){
     setHealth(5);
     setStrength(2);
     setLevel(1);
     setDefense(1);
 
+    }*/
+
+//unfinished, still needs to be able to modify the player's stats, and need to work out
+//  how to get the player to choose what they need.
+void updateStats(int statPointsTotal)
+{
+  player playerEdit;
+  bool correctAnswer = false;
+  int answer;
+
+  while (!correctAnswer)
+    {
+      cout << "1 to view current stats.\n2 to update stats.\n3 to exit.\nYou have "
+           << statPointsTotal << " stat points left.\n";
+      cout << "Enter answer: ";
+      cin >> answer;
+
+      if (answer == 1)
+        {
+          cout << "Strength - " << getStrength()
+               << "\nDexterity - "
+               << "\nConstitution - "
+               << "\nIntelligence - "
+               << "\nWisdom - "
+               << "\nCharisma - "
+               << "\n\n";
+        }
+      else if (answer == 2)
+        {
+          cout << "Caution! All edits made to stat points are final.\nChoose wisely.\n";
+          if (statPointsTotal > 0)
+            {
+          //correctAnswer here is used to determine if the player wants to exit.
+          while (statPointsTotal > 0 || correctAnswer == false)
+            {
+              cout << "1 to upgrade strength.\n"
+                   << "2 to upgrade dexterity.\n"
+                   << "3 to upgrade constitution.\n"
+                   << "4 to upgrade intelligence.\n"
+                   << "5 to upgrade wisdom.\n"
+                   << "6 to upgrade charisma.\n"
+                   << "7 to exit.\n";
+                   << "Enter answer: ";
+              cin >> answer;
+
+              if (answer == 1)
+                {
+
+                }
+              else if (answer == 2)
+                {
+
+                }
+              else if (answer == 3)
+                {
+
+                }
+              else if (answer == 4)
+                {
+
+                }
+              else if (answer == 5)
+                {
+
+                }
+              else if (answer == 6)
+                {
+                  cout << "You have selected to upgrade charisma.\n";
+                  setCharisma(getCharisma() + 1);
+                }
+              else if (answer == 7)
+                {
+                  cout << "Exiting...\n";
+                }
+              else
+                {
+                  cout << "Invalid response. Enter a correct response.\n"
+                }
+            }
+          cout << "You have finished updating your skills.\n";
+            }
+          else
+            cout << "You have no stat points to upgrade your skills.\n";
+        }
+      else if (answer == 3)
+        {
+          //exits the while loop
+          correctAnswer = true;
+        }
+      else
+        correctAnswer = false;
+    }
 }
 
+//unfinished
+void characterCreation()
+{
+  //defines an object player
+  player playerEdit;
+  int answer;
+  bool correctAnswer = false;
+
+  //player determines if they want to create their own, or to have a random one
+  cout << "1 to create your own character\n"
+       << "2 to randomly generate a character.\nEnter answer:";
+  cin >> answer;
+
+  if (answer == 1)
+    {
+      while (!correctAnswer)
+        {
+          cout << "Press 1 for mage, or 2 for fighter: ";
+          cin >> answer;
+
+          if (answer == 1)
+            {
+              cout << "You have selected mage.\n";
+              correctAnswer = true;
+            }
+          else if (answer == 2)
+            {
+              cout << "You have selected fighter.\n";
+              correctAnswer = true;
+            }
+          else
+            {
+              cout << "Invalid answer, please try again.\n";
+              correctAnswer = false;
+            }
+        }
+
+      //resets correctAnswer for the next question.
+      correctAnswer = false;
+
+      //determines stats
+      cout << "You are allowed to increase your base stats.\n";
+      updateStats(6);
+    }
+
+  //creates a random character
+  else if (answer == 2)
+    {
+      //determines the player's class
+      if (true)
+        cout << "Congratulations, you are a mage!\n";
+      else
+        cout << "Congratulations, you are a fighter!\n";
+    }
+  else
+    {
+      cout << "Error encountered: Invalid input.\n";
+    }
+}
+
+//END OF PLAYER FUNCTIONS
 
 class wolf : public creatureType{
     public:
